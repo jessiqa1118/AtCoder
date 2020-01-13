ffc2f954295c87988879af7fb5ebd4181643615f
じぇしか <jscmla1118@gmail.com>
2020-01-13 20:32:03 +0900

    Add answer

---
+++ AtCoder_Beginners_Selection/Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split(' ');
+        int a = int.Parse(input[0]);
+        int b = int.Parse(input[1]);
+
+        if ((a * b) % 2 == 0)
+        {
+            Console.WriteLine("Even");
+        } else {
+            Console.WriteLine("Odd"); 
+        }
+    }
+}
