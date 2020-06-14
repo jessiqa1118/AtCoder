68087ef3713aed518b58ccf313b5d12ae38d6931
jscmla <jscmla1118@gmail.com>
2020-06-14 22:50:08 +0900

    feat: add answer for B problem in C++

---
+++ ABC170/B/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main()
+{
+    int X, Y;
+    cin >> X >> Y;
+
+    rep(i, X + 1)
+    {
+        if ((i * 2 + (X - i) * 4) == Y)
+        {
+            cout << "Yes" << endl;
+            return 0;
+        }
+    }
+
+    cout << "No" << endl;
+}
