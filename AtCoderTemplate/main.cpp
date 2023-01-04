ee9455c164655eac489d6e6891b38cac2288ef00
じぇしか <jscmla1118@gmail.com>
2023-01-04 21:03:21 +0900

    Add include statement for bits/stdc++.h

--- AtCoderTemplate/main.cpp
+++ AtCoderTemplate/main.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, AtCoder Template!" << std::endl;
-    return 0;
+    cout << "Hello, AtCoder Template!" << endl;
 }
