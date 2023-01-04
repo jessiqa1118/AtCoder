500bf02c61ff9bc247313800708be6fef4ae962b
じぇしか <jscmla1118@gmail.com>
2023-01-04 23:31:30 +0900

    Update answer of ABC081A in C++

--- ABS/ABC081A/main.cpp
+++ ABS/ABC081A/main.cpp
@@ -1,19 +1,15 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main()
-{
-    string input;
-    int cnt = 0;
-    cin >> input;
+int main() {
+    string S;
+    cin >> S;
 
-    for (int i = 0; i < input.size(); i++)
+    int cnt = 0;
+    for (int i = 0; i < S.length(); i++)
     {
-        if (input[i] == '1')
-        {
-            cnt++;
-        }
+        cnt += (S.substr(i, 1) == "1") ? 1 : 0;
     }
 
     cout << cnt << endl;
