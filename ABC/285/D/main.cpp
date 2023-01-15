48f46a60d14b6e2e94459030fd5d683af742eaa4
じぇしか <jscmla1118@gmail.com>
2023-01-15 23:27:02 +0900

    Add answer for D of ABC285 in C++

--- ABC/285/D/main.cpp
+++ ABC/285/D/main.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 #define ll long long
 
-vector<int> par;
+map<string, string> parent;
 
-int get_root(int index) {
-    if (par[index] == index) return index;
-    return par[index] = get_root(par[index]);
+string get_root(string key) {
+    if (parent[key] == key) return key;
+    return parent[key] = get_root(parent[key]);
 }
 
 int main() {
@@ -28,23 +28,19 @@ int main() {
     unique(V.begin(), V.end());
 
     // Initialize roots
-    par.resize((int) V.size());
     for (int i = 0; i < (int) V.size(); i++) {
-        par[i] = i;
+        parent[V[i]] = V[i];
     }
 
     for (int i = 0; i < N; i++) {
-        auto is = distance(V.begin(), find(V.begin(), V.end(), S[i]));
-        auto it = distance(V.begin(), find(V.begin(), V.end(), T[i]));
-
         // Check group
-        if (get_root(is) == get_root(it)) {
+        if (get_root(S[i]) == get_root(T[i])) {
             cout << "No" << endl;
             return 0;
         }
 
         // Update roots
-        par[it] = get_root(is);
+        parent[T[i]] = get_root(S[i]);
     }
 
     cout << "Yes" << endl;
