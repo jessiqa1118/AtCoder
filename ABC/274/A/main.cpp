04a806579f412fc122e057932109ee8aedd65bda
じぇしか <jscmla1118@gmail.com>
2023-02-13 23:31:09 +0900

    Add answer for A of ABC274 in C++

---
+++ ABC/274/A/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+
+    cout << fixed << setprecision(3) << (double) B / A << endl;
+}
