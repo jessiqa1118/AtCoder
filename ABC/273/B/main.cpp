c91ff614cf8ccae9c3b7ec1f74fb4750da5a8260
じぇしか <jscmla1118@gmail.com>
2023-02-22 23:24:34 +0900

    Add answer for B of ABC273 in C++

--- ABC/273/B/main.cpp
+++ ABC/273/B/main.cpp
@@ -4,35 +4,27 @@ using namespace std;
 
 #define ll long long
 
+ll pow10(int n) {
+    ll p = 1;
+    for (int i = 0; i < n; i++) {
+        p *= 10;
+    }
+
+    return p;
+}
+
 int main() {
-    string X;
+    ll X;
     int K;
     cin >> X >> K;
 
-    if ((int) X.size() < K) {
-        cout << 0 << endl;
-        return 0;
-    }
-
-    reverse(X.begin(), X.end());
-
-    string ans = "";
     int carry = 0;
-    for (int i = 0; i < K; i++) {
-        ans += '0';
-
-        int value = (int) (X[i] - '0') + carry;
-        carry = (value >= 5) ? 1 : 0;
+    for (int i = 0; i <= K; i++) {
+        X /= pow10(i);
+        X += carry;
+        carry = ((X % 10) >= 5) ? 1 : 0;
+        X *= pow10(i);
     }
 
-    for (int i = K; i < (int) X.size(); i++) {
-        int value = (int) (X[i] - '0') + carry;
-        carry = value / 10;
-        ans += (char) (value % 10) + '0';
-    }
-
-    if (carry > 0) ans += (char) carry + '0';
-
-    reverse(ans.begin(), ans.end());
-    cout << ans << endl;
+    cout << X << endl;
 }
